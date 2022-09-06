31ee45aaa5df737241f5216541f0ed6f8be50acf
Holly Gardner <hollylizgardner@gmail.com>
2022-09-06 17:56:31 -0400

    Add a GET route to show form

--- express_server.js
+++ express_server.js
@@ -4,10 +4,27 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {  
+let result = ""
+let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+charactersLength = characters.length
+for (let i = 0; i < length; i++) {
+  result += characters.charAt(Math.floor(Math.random() * charactersLength))
+}
+return result
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -28,6 +45,10 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
