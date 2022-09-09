3585d5bd2fa390c62a37cdd43039c8d99fde596d
Holly Gardner <hollylizgardner@gmail.com>
2022-09-09 12:05:30 -0400

    Helper function to determine if userID is equal to id of current user

--- helpers.js
+++ helpers.js
@@ -6,4 +6,15 @@ const getUserByEmail = function(emailAddress, users) {
   }
 };
 
-module.exports = { getUserByEmail };
+const urlsForUserId = function(id, URLdatabase) {
+  let userURLS = {};
+  for (let url in URLdatabase) {
+    if (URLdatabase[url].userID === id) {
+      userURLS[url] = URLdatabase[url];
+    }
+  }
+  return userURLS;
+}
+
+
+module.exports = { getUserByEmail, urlsForUserId };
